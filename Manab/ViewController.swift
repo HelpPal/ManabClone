69321e3912e7b7983ed4185643e5a2c2480191ed
aaaa bbbb <username@email.com>
2018-09-26 20:51:59 +0900

    First update

---
+++ Manab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Manab
+//
+//  Created by 123 on 2018/09/26.
+//  Copyright © 2018 Lily. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
